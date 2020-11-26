83d414e883dfcedd783555d2b8fe350e142d245d
faruckchavarria <faruckchavarria2@gmail.com>
2020-11-25 20:08:49 -0600

    se realiza el primero ejercicio de los nombre, apellidos y edad, una disculpa por agregar lo demas, se me fue desapersivido

---
+++ Tipos.kt
@@ -0,0 +1,21 @@
+fun main ()
+{
+    //numeros
+    val int = 123
+    val long = 123456L
+    val double = 12.34
+    val float = 12.34F
+    val hexadecimal = 0xAB
+    val binary = 0b10101010101
+
+    println (" los valores de las variables son entero $int entero mas largo $long variable doble $double variable decimal $float variable hexadecimal $hexadecimal variable binaria $binary")
+
+    //codigo numero 2
+    //boleanos
+    val x = 1
+    val y = 2
+    val z = 2
+    val esTrue = x < y && x < z
+    val otroTrue = x == y || y == z
+
+}
---
+++ Variables.kt
@@ -0,0 +1,17 @@
+fun main ()
+{
+    var nombre = "Faruck" 
+    var edad = "20"
+
+    nombre = "jose"
+    println(nombre)
+    //println("mi nombre es $nombre $apellido y tengo $edad años")
+
+    var apellido = "Chavarria" 
+
+    apellido = "Balladares"
+    println(apellido)
+    //EL error mostrado es que se declara la variable con val la cual no permite cambiar el valor de la variable, y debemos cambiarlo a VAR para que pueda funcionar, realice el cambio a VAR
+
+    println ("mi nombre es $nombre $apellido y tengo $edad años")
+}
