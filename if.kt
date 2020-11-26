4ffc4c035ffa7d8e555501942fcfae7f388572cc
faruckchavarria <faruckchavarria2@gmail.com>
2020-11-26 10:46:34 -0600

    se ingresa la edad atraves del teclado

--- if.kt
+++ if.kt
@@ -1,21 +1,22 @@
 import java.util.Date;
 fun main()
 {
- var fecha = Date()
-var hoy = if (fecha.year == 2019) true else false
+  //  var fecha = Date()
+//var hoy = if (fecha.year == 2019) true else false
 
+//println(hoy)
+//println(fecha)
 //utulice la libreia de date de java import para poder hacer eso de la clase date en kotlin
 
-val edad = 20
+
+print("ingrese la edad:")
+var edad = readLine()!!.toInt()
 if(edad >= 20)
 {
  println("eres mayor de edad")
 } else {
  println("aun eres menor")
-
- println(hoy)
- println(fecha)
- }
+}
 // para poder ejecutar este codigo debvo comentra el anterior para asi poder mostrar los resultados
 
 
