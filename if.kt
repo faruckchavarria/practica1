3c4c08ab82a2c4ecb8ae11791fcf7e41668ab95f
faruckchavarria <faruckchavarria2@gmail.com>
2020-11-26 09:57:53 -0600

    Se agrega el fichero de if.kt

---
+++ if.kt
@@ -0,0 +1,22 @@
+import java.util.Date;
+fun main()
+{
+ var fecha = Date()
+var hoy = if (fecha.year == 2019) true else false
+
+//utulice la libreia de date de java import para poder hacer eso de la clase date en kotlin
+
+val edad = 20
+if(edad >= 20)
+{
+ println("eres mayor de edad")
+} else {
+ println("aun eres menor")
+
+ println(hoy)
+ println(fecha)
+ }
+// para poder ejecutar este codigo debvo comentra el anterior para asi poder mostrar los resultados
+
+
+}
